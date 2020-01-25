a99c09c97883b4457837f3e1ec89b8f572061bb1
TakeoffAndroid <takeoffandroid@gmail.com>
2020-01-25 10:48:35 +0530

    Adds cart repository sample test

--- test/features/cart/data/repositories/cart_repository_impl_test.dart
+++ test/features/cart/data/repositories/cart_repository_impl_test.dart
@@ -1,2 +1,13 @@
-// TODO(Sayeed): Add tests
-void main() {}
+import 'package:flutter_test_pyramid_spike/features/cart/data/repositories/cart_repository_impl.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test(
+      'SHOULD return empty cart items WHILE acessing the cart items without adding the product', () async {
+    final cartRepositoryImpl = CartRepositoryImpl();
+
+    expect(cartRepositoryImpl.getCartItems().length, 0);
+  });
+
+}
+
