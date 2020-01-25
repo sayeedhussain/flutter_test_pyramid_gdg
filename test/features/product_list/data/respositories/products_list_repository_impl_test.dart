dca73f2c8375dd3fe9b05e553b02ed8fea529631
TakeoffAndroid <takeoffandroid@gmail.com>
2020-01-25 12:54:26 +0530

    Adds mockito unit test for products list repository

--- test/features/product_list/data/respositories/products_list_repository_impl_test.dart
+++ test/features/product_list/data/respositories/products_list_repository_impl_test.dart
@@ -1,3 +1,31 @@
-// TODO(Sayeed): Add Unit Tests
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test_pyramid_spike/features/product_list/data/models/product_model.dart';
+import 'package:flutter_test_pyramid_spike/features/product_list/data/repositories/products_list_repository_impl.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
 
-void main() {}
+import '../../mocks/mock_products_list_remote_data_source.dart';
+
+void main() {
+
+  MockProductsListRemoteDataSource mockProductsRemoteDataSource;
+
+  setUp(() {
+    mockProductsRemoteDataSource = MockProductsListRemoteDataSource();
+  });
+
+  test('SHOULD fetch the producs WHEN the api call is success', () async {
+    final productListRepository = ProductsListRepositoryImpl(remoteDataSource: mockProductsRemoteDataSource);
+
+    final productModels = [(getProductModel())];
+
+    when(mockProductsRemoteDataSource.getProducts()).thenAnswer((_) async => productModels);
+
+    final actualProducts = await productListRepository.getProducts();
+    expect(actualProducts, equals(Right(productModels)));
+  });
+}
+
+ProductModel getProductModel({String upc = 'upc', String name = 'name', String price = 'price', int commentsCount = 0,
+  String image = 'image'}) => ProductModel(upc: upc, name: name, price: price, commentsCount: commentsCount, image: image);
+
---
+++ test/features/product_list/mocks/mock_products_list_remote_data_source.dart
@@ -0,0 +1,4 @@
+import 'package:flutter_test_pyramid_spike/features/product_list/data/datasources/product_list_remote_datasource.dart';
+import 'package:mockito/mockito.dart';
+
+class MockProductsListRemoteDataSource extends Mock implements ProductListRemoteDataSource{}
